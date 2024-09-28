e5b973448af729696690880aea1f893dfa933d27
Surapat-SV <surapatpine@gmail.com>
2024-09-28 11:39:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,6 @@ if gemini_api_key:
         # Configure Gemini with the provided API Key
         genai.configure(api_key=gemini_api_key)
         model = genai.GenerativeModel("gemini-pro")
-        respond = model.generate_content('Introduce yourself as "N\'Assist," a digital marketing intern.')
         st.success("Gemini API Key successfully configured.")
     except Exception as e:
         st.error(f"An error occurred while setting up the Gemini model: {e}")
