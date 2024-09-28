c166676695818b1e56b35d1564f1e553707ba57f
Surapat-SV <surapatpine@gmail.com>
2024-09-28 10:22:54 +0700

    Update streamlit_app.py
    fix weird ui

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,12 +7,12 @@ st.subheader("Conversation")
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = []  # Initialize with an empty list
 
-# Display the user input
-if user_input := st.chat_input("What's on your minds: "):
-    st.session_state.chat_history.append(user_input)
-    st.chat_message("user").markdown(user_input)
-    
-# Display all messages using st.write
+# Display all chat messages
 for message in st.session_state.chat_history:
-    st.chat_message(message)
-    st.markdown(message)
+    with st.chat_message("user"):
+        st.markdown(message)
+
+# Capture user input and append to chat history
+if prompt := st.chat_input("Type your message here ..."):
+    st.session_state.chat_history.append(prompt)
+    st.chat_message("user").markdown(prompt)
