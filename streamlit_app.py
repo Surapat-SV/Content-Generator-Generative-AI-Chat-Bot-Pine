0fe4e53a4fedcc29a88a62d429df28fcc8a46476
Surapat-SV <surapatpine@gmail.com>
2024-09-28 10:02:21 +0700

    Update streamlit_app.py
    update to chat history

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,16 @@
 import streamlit as st
 
 st.title("🐱 My chatbot app")
+st.subheader("Conversation")
+
+# Initialize session state for storing chat history
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []  # Initialize with an empty list
 
 # Display the user input
-if user_input := st.text_input("What's on your mind", placeholder="Type your message here..."):
-    st.write(f"User Input: {user_input}")
+if user_input := st.text_input("What's on your minds: ", placeholder="Type your message here..."):
+    st.session_state.chat_history.append(user_input)
+# Display all messages using st.write
+
+for message in st.session_state.chat_history:
+    st.write(message)
