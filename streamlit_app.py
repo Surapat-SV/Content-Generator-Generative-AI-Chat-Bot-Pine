2113ad423b3c5330a4c0c9a6a73d62ad94138bba
Surapat-SV <surapatpine@gmail.com>
2024-09-28 09:54:07 +0700

    Update streamlit_app.py
    change emoji to cat

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-st.title("🧸 My chatbot app")
+st.title("🐱 My chatbot app")
 
 # Display the user input
-if user_input := st.text_input("You: ", placeholder="Type your message here..."):
+if user_input := st.text_input("What's on your mind", placeholder="Type your message here..."):
     st.write(f"User Input: {user_input}")
